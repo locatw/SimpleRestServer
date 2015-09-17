c77f4e110b25428c96c90d4003b5ce1754e297ca
locatw <loca.gm@gmail.com>
2015-09-17 23:35:22 +0900

    create HttpRequest class can parse first line in request header

---
+++ SimpleRestServer/SimpleRestServer.Tests/HttpRequestTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestServer.Tests
+{
+    public class HttpRequestTest
+    {
+        [TestClass]
+        public class SimpleRequestHeaderParseTest
+        {
+            private readonly string requestText =
+                "GET / HTTP/1.1" + "\r\n" +
+                "Host: sample.jp" + "\r\n";
+
+            [TestMethod]
+            public void RequestMethodIsGet()
+            {
+                var request = new HttpRequest(requestText);
+
+                Assert.AreEqual(HttpRequestMethod.Get, request.Method);
+            }
+
+            [TestMethod]
+            public void VersionIs1_1()
+            {
+                var request = new HttpRequest(requestText);
+
+                Assert.AreEqual(HttpVersion.Version1_1, request.Version);
+            }
+
+            [TestMethod]
+            public void RequestUriIsRoot()
+            {
+                var request = new HttpRequest(requestText);
+
+                Assert.AreEqual("/", request.Uri);
+            }
+        }
+    }
+}
---
+++ SimpleRestServer/SimpleRestServer/HttpRequest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace SimpleRestServer
+{
+    public class HttpRequest
+    {
+        public HttpRequest(string request)
+        {
+            Parse(request);
+        }
+
+        public HttpRequestMethod Method { get; private set; }
+
+        public HttpVersion Version { get; private set; }
+
+        public string Uri { get; private set; }
+
+        private void Parse(string request)
+        {
+            string[] lines = request.Split('\n')
+                                .Select(s => s.Replace("\r", ""))
+                                .ToArray();
+
+            ParseRequestLine(lines[0]);
+        }
+
+        private void ParseRequestLine(string line)
+        {
+            string[] elems = line.Split(' ');
+
+            HttpRequestMethod method;
+            if (Enum.TryParse(elems[0], true, out method))
+            {
+                this.Method = method;
+            }
+            else
+            {
+                throw new Exception("unknown request method.");
+            }
+
+            this.Uri = elems[1];
+
+            switch (elems[2])
+            {
+                case "HTTP/1.1":
+                    this.Version = HttpVersion.Version1_1;
+                    break;
+                default:
+                    throw new Exception("unsupported http version");
+            }
+        }
+    }
+
+    public enum HttpRequestMethod
+    {
+        Get
+    }
+
+    public enum HttpVersion
+    {
+        Version1_1
+    }
+
+    public enum HttpRequestHeaderField
+    {
+        Host
+    }
+}
