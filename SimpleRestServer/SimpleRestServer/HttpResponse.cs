b4267195823147a333a53ed0ec4c141487b30643
locatw <loca.gm@gmail.com>
2015-09-19 21:16:15 +0900

    create class stands for HTTP response

---
+++ SimpleRestServer/SimpleRestServer/HttpResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestServer
+{
+    public class HttpResponse
+    {
+        public HttpResponse(HttpStatus status)
+        {
+            Status = status;
+        }
+
+        public HttpStatus Status { get; private set; }
+    }
+}
