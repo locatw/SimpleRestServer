fcc6aacc4bf0e581d47e859a1ceb5a9f8888defd
locatw <loca.gm@gmail.com>
2015-09-19 21:02:08 +0900

    create class stands for HTTP status code

---
+++ SimpleRestServer/SimpleRestServer.Tests/HttpStatusTest.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestServer.Tests
+{
+    public class HttpStatusTest
+    {
+        [TestClass]
+        public class EqualityTest
+        {
+            [TestMethod]
+            public void EqualsMethodReturnsTrueWhenSameStatusCodeAndDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "OK");
+
+                Assert.IsTrue(status1.Equals(status2));
+            }
+
+            [TestMethod]
+            public void EqualsMethodReturnsFalseWhenSameStatusCodeButDifferentDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "Success");
+
+                Assert.IsFalse(status1.Equals(status2));
+            }
+
+            [TestMethod]
+            public void EqualsMethodReturnsFalseWhenSameDescriptionButDifferentStatusCode()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(201, "OK");
+
+                Assert.IsFalse(status1.Equals(status2));
+            }
+
+            [TestMethod]
+            public void OperatorEqualReturnsTrueWhenSameStatusCodeAndDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "OK");
+
+                Assert.IsTrue(status1 == status2);
+            }
+
+            [TestMethod]
+            public void OperatorEqualReturnsFalseWhenSameStatusCodeButDifferentDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "Success");
+
+                Assert.IsFalse(status1 == status2);
+            }
+
+            [TestMethod]
+            public void OperatorEqualReturnsFalseWhenSameDescriptionButDifferentStatusCode()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(201, "OK");
+
+                Assert.IsFalse(status1 == status2);
+            }
+
+            [TestMethod]
+            public void OperatorNotEqualReturnsFalseWhenSameStatusCodeAndDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "OK");
+
+                Assert.IsFalse(status1 != status2);
+            }
+
+            [TestMethod]
+            public void OperatorNotEqualReturnsTrueWhenSameStatusCodeButDifferentDescription()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(200, "Success");
+
+                Assert.IsTrue(status1 != status2);
+            }
+
+            [TestMethod]
+            public void OperatorNotEqualReturnsTrueWhenSameDescriptionButDifferentStatusCode()
+            {
+                var status1 = new HttpStatus(200, "OK");
+                var status2 = new HttpStatus(201, "OK");
+
+                Assert.IsTrue(status1 != status2);
+            }
+        }
+    }
+}
---
+++ SimpleRestServer/SimpleRestServer/HttpStatus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SimpleRestServer
+{
+    public class HttpStatus
+    {
+        public static readonly HttpStatus Ok = new HttpStatus(200, "OK");
+
+        public HttpStatus(int code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as HttpStatus);
+        }
+
+        public bool Equals(HttpStatus obj)
+        {
+            if (Object.ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return Code == obj.Code && Description.Equals(obj.Description);
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode() ^ Description.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", Code, Description);
+        }
+
+        public static bool operator ==(HttpStatus obj1, HttpStatus obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(HttpStatus obj1, HttpStatus obj2)
+        {
+            return !obj1.Equals(obj2);
+        }
+
+        public int Code { get; private set; }
+
+        public string Description { get; private set; }
+    }
+}
