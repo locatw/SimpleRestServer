45f8a2b7e3b1dca9c2ca33141ec42c2afb555e3f
locatw <loca.gm@gmail.com>
2015-09-23 11:01:50 +0900

    create sample application projects for test

---
+++ SimpleRestServer/SampleApp.BackgroundService/ServerTask.cs
@@ -0,0 +1,87 @@
+﻿using SimpleRestServer;
+using System;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace SampleApp.BackgroundService
+{
+    public sealed class ServerTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral serviceDeferral;
+
+        private AppServiceConnection appServiceConnection;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            taskInstance.Canceled += OnCanceled;
+
+            serviceDeferral = taskInstance.GetDeferral();
+
+            var appService = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            if (appService != null &&
+                appService.Name == "RestServerService")
+            {
+                appServiceConnection = appService.AppServiceConnection;
+                appServiceConnection.RequestReceived += OnRequestReceived;
+            }
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            var message = args.Request.Message;
+            string command = message["Command"] as string;
+
+            switch (command)
+            {
+                case "Initialize":
+                    {
+                        var messageDeferral = args.GetDeferral();
+
+                        var config = new ServerConfig() { Port = 8000 };
+                        var server = new RestServer(config);
+                        server.Route("/", RootAction);
+
+                        IAsyncAction asyncAction = Windows.System.Threading.ThreadPool.RunAsync(
+                            async workItem =>
+                            {
+                                await server.RunAsync();
+                            });
+
+                        var returnMessage = new ValueSet();
+                        returnMessage.Add("Status", "Success");
+
+                        var responseStatus = await args.Request.SendResponseAsync(returnMessage);
+
+                        messageDeferral.Complete();
+
+                        break;
+                    }
+
+                case "Quit":
+                    {
+                        serviceDeferral.Complete();
+                        break;
+                    }
+            }
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        { }
+
+        private HttpResponse RootAction(HttpRequest request)
+        {
+            string body = @"
+<html>
+    <header><title>SampleApp</title></header>
+    <body>SampleApp</body>
+</html>";
+
+            var response = new HttpResponse(HttpVersion.Version1_1, HttpStatus.Ok);
+            response.SetContent(body);
+
+            return response;
+        }
+    }
+}
---
+++ SimpleRestServer/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace SampleApp
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private AppServiceConnection appServiceConnection = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            appServiceConnection = new AppServiceConnection();
+            appServiceConnection.PackageFamilyName = Package.Current.Id.FamilyName;
+            appServiceConnection.AppServiceName = "RestServerService";
+
+            var result = await appServiceConnection.OpenAsync();
+            if (result == AppServiceConnectionStatus.Success)
+            {
+                var message = new ValueSet();
+                message.Add("Command", "Initialize");
+
+                var response = await appServiceConnection.SendMessageAsync(message);
+                if (response.Status != AppServiceResponseStatus.Success)
+                {
+                    throw new Exception("failed to send message");
+                }
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (appServiceConnection != null)
+            {
+                appServiceConnection.Dispose();
+            }
+        }
+    }
+}
